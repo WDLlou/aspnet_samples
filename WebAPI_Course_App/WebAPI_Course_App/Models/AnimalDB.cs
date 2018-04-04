a8be19a8c61f256e2f70f00baf9451a50de4165c
Ekaterina B <ekaterina.balaban@gmail.com>
2018-04-04 14:43:51 -0400

    Added a new entry to the AnimalDB.cs

--- WebAPI_Course_App/WebAPI_Course_App/Models/AnimalDB.cs
+++ WebAPI_Course_App/WebAPI_Course_App/Models/AnimalDB.cs
@@ -13,7 +13,8 @@ namespace WebAPI_Course_App.Models
             new Animal() {Species = "Cat", NumberOfLegs = 4, Genus = "Feline"},
             new Animal() {Species = "Cow", NumberOfLegs = 4, Genus = "Bovine"},
             new Animal() {Species = "Bee", NumberOfLegs = 6, Genus = "Apis"},
-            new Animal() {Species = "Spider", NumberOfLegs = 8, Genus = "Arachnid"}
+            new Animal() {Species = "Spider", NumberOfLegs = 8, Genus = "Arachnid"},
+            new Animal() {Species = "Wolf", NumberOfLegs = 4, Genus = "Canine"}
         };
     }
 }
